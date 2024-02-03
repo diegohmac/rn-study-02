9f291e714ee04fe3fedf4f0dc2c82fad22b3d7ee
Diego Machado <machadodiegoh@gmail.com>
2024-02-03 14:28:16 +0000

    initial setup

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Container, Title } from './styles';
+
+export default function App() {
+  return (
+    <Container>
+      <Title>Groups</Title>
+      <StatusBar style="light" />
+    </Container>
+  );
+}
