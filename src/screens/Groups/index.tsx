42b5acdbae921e1291a4066e08043758172e1fb7
Diego Machado <machadodiegoh@gmail.com>
2024-02-04 07:57:12 +0000

    add Group Card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
 
 export default function App() {
   return (
     <Container>
       <Header />
       <Highlight title='Teams' subtitle='play with your team' />
+      <GroupCard title='Test Team' />
     </Container>
   );
 }
